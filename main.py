aae2e46d4ecadc7f5741c108832f927b3dbe2e9e
DaveStudio7 <dstudio70@yahoo.com>
2025-06-18 11:06:41 -0400

    updated bookbot

--- main.py
+++ main.py
@@ -1,43 +1,35 @@
-from stats import get_num_words
+from stats import (
+    get_num_words,
+    chars_dict_to_sorted_list,
+    get_chars_dict,
+)
+
 
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    char_dict = get_num_characters(text)
-
-    char_list = []
-    for char, count in char_dict.items():
-        char_list.append({"char": char, "num": count})
-    char_list.sort(reverse=True, key=sort_on)
-
-    print(f"--- Begin report of {book_path} ---")
-    print(f"{num_words} words found in the document")
-    print()
-
-    for char_info in char_list:
-        print(f"The '{char_info['char']}' character was found {char_info['num']} times")
-        
-    print("--- End report ---")
-
-
-def sort_on(dict):
-    return dict["num"]
-
-
-def get_num_characters(text):
-    char_count = {}
-    lower_char = text.lower()
-    for char in lower_char:
-        if char.isalpha():
-            if char in char_count:
-                char_count[char] += 1
-            else:
-                char_count[char] = 1
-    return char_count
+    chars_dict = get_chars_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+    print_report(book_path, num_words, chars_sorted_list)
 
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+
+
+def print_report(book_path, num_words, chars_sorted_list):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"{item['char']}: {item['num']}")
+
+    print("============= END ===============")
+
 main()
